9da7f626cb2ee652af9e85842709cf80895835e8
Olena Malyshko <olena.malyshko@haw-hamburg.de>
2014-04-30 01:20:02 +0200

    Referent class added

---
+++ Referent.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Referent{
+    private String name;
+    private String email;
+    private String firma;
+	private boolean istAngemeldet;
+    private ArrayList<Workshop>workshopListe;
+
+    public Referent(String name, String email, String firma){
+        this.name = name;
+        this.email = email;
+        this.firma = firma;
+    }
+
+    public void addWorkshop(Workshop workshop){
+        workshopListe.add(workshop);
+    }
+
+    public String showWorkshopListe(){
+        return "Workshopangebot von " + name + ": " + workshopListe;
+    }
+
+    public boolean anmelden(){
+        if(istAngemeldet==false){
+            istAngemeldet = true;
+        }
+        return true;
+    }
+
+    public String toString(){
+        return "Name: " + name + "; E-Mail: " + email + "; Firma: " + firma + "\n";
+    }
+}
